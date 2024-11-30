1d161f55ca2315b42309fff5499c8b127beb4d50
hu6r1s <darmaa333@gmail.com>
2024-11-30 23:12:19 +0900

    feat: hold active button

--- popup.js
+++ popup.js
@@ -3,3 +3,13 @@ $('#welcome_URL').attr('href', `chrome-extension://${chrome.runtime.id}/home.htm
 $("#onoffbox").on("click", () => {
   chrome.storage.local.set({ "chronoEnable": $("#onoffbox").is(":checked") });
 });
+
+chrome.storage.local.get("chronoEnable", (result) => {
+  if (result.chronoEnable === undefined) {
+    $("#onoffbox").prop("checked", true);
+    chrome.storage.local.set({ "chronoEnable": $("#onoffbox").is(":checked") });
+  } else {
+    $("#onoffbox").prop("checked", result.chronoEnable);
+    chrome.storage.local.set({ "chronoEnable": $("#onoffbox").is(":checked") });
+  }
+});
