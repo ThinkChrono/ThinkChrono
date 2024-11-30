2a88c873380b1731f96def5ec6aab936cd0bc171
hu6r1s <darmaa333@gmail.com>
2024-11-30 23:05:22 +0900

    feat: Activate ThinkChrono

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
 $('#welcome_URL').attr('href', `chrome-extension://${chrome.runtime.id}/home.html`);
+
+$("#onoffbox").on("click", () => {
+  chrome.storage.local.set({ "chronoEnable": $("#onoffbox").is(":checked") });
+});
