28fdf69267c1148d30b0987881062ebd21f8c1c7
hu6r1s <darmaa333@gmail.com>
2024-12-21 13:35:46 +0900

    feat: add goorm platform

--- scripts/allowedDomain.js
+++ scripts/allowedDomain.js
@@ -1,5 +1,6 @@
 const ALLOWED_DOMAINS = Object.values(Object.freeze({
   BAEKJOON: "www.acmicpc.net",
   PROGRAMMERS: "school.programmers.co.kr",
-  SWEA: "swexpertacademy.com"
+  SWEA: "swexpertacademy.com",
+  GOORM: "level.goorm.io"
 }));
