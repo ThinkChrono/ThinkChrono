9b16274896bd23d1abfbe7fafe8dd060ddaa340f
hu6r1s <darmaa333@gmail.com>
2024-12-21 13:39:09 +0900

    feat: add leetcode platform

--- scripts/allowedDomain.js
+++ scripts/allowedDomain.js
@@ -2,5 +2,6 @@ const ALLOWED_DOMAINS = Object.values(Object.freeze({
   BAEKJOON: "www.acmicpc.net",
   PROGRAMMERS: "school.programmers.co.kr",
   SWEA: "swexpertacademy.com",
-  GOORM: "level.goorm.io"
+  GOORM: "level.goorm.io",
+  LEETCODE: "leetcode.com"
 }));
