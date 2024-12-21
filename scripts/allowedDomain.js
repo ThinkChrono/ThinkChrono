e2a09caabdb99361d05309db43b354e8b8bc5fe5
hu6r1s <darmaa333@gmail.com>
2024-12-21 13:31:04 +0900

    feat: add swea platform

--- scripts/allowedDomain.js
+++ scripts/allowedDomain.js
@@ -1,4 +1,5 @@
 const ALLOWED_DOMAINS = Object.values(Object.freeze({
   BAEKJOON: "www.acmicpc.net",
-  PROGRAMMERS: "school.programmers.co.kr"
+  PROGRAMMERS: "school.programmers.co.kr",
+  SWEA: "swexpertacademy.com"
 }));
